882df757f99628f1237a92e03de82ae5b93d26c5
Penguin <149217055+Potalzz@users.noreply.github.com>
2026-02-19 21:40:35 +0900

    Assignment1.0 implement Task 2

--- assignments/1.0_DrawSVG/DrawSVG/src/software_renderer.cpp
+++ assignments/1.0_DrawSVG/DrawSVG/src/software_renderer.cpp
@@ -244,6 +244,32 @@ void SoftwareRendererImp::rasterize_line( float x0, float y0,
 
   // Task 2: 
   // Implement line rasterization
+  // 어떤 경사도의 선도 처리
+  // O(length of line) 시간 복잡도
+
+  // Bresenhams's line algorithm을 통해 구현
+  int dx = abs(x1 - x0);
+  int sx = (x0 < x1) ? 1 : -1;
+
+  int dy = -abs(y1 - y0);
+  int sy = (y0 < y1) ? 1 : -1;
+  
+  int err = dx + dy;
+
+  while (true) {
+    rasterize_point(x0, y0, color);
+    if (x0 == x1 && y0 == y1) break;
+    int e2 = 2 * err;
+    if (e2 >= dy) { 
+      err += dy; 
+      x0 += sx; 
+      
+    }
+    if (e2 <= dx) { 
+      err += dx; 
+      y0 += sy; 
+    }
+  }
 }
 
 void SoftwareRendererImp::rasterize_triangle( float x0, float y0,
